281678e2b70d58efa27bd2bfbdd89f33cb6239fb
ytnobody <ytnobody@gmail.com>
2011-03-24 18:32:44 +0900

    initial commit

---
+++ lib/Upas/Client.pm
@@ -0,0 +1,32 @@
+package Upas::Client;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Upas::Client -
+
+=head1 SYNOPSIS
+
+  use Upas::Client;
+
+=head1 DESCRIPTION
+
+Upas::Client is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
